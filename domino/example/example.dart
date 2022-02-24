1699500b4e5befb4a8d0cbbf1f0ac19897f73f25
Oliver Damm <oliver.damm@gmx.de>
2022-02-24 21:14:34 +0100

    (re)introduce a class to hold state in the example; bump package version

--- domino/example/example.dart
+++ domino/example/example.dart
@@ -3,26 +3,28 @@ import 'dart:html';
 import 'package:domino/browser.dart';
 
 void main() {
-  registerView(root: document.getElementById('root')!, builderFn: _app);
+  registerView(root: document.getElementById('root')!, builderFn: _App().build);
 }
 
-int _counter = 0;
+class _App {
+  int _counter = 0;
 
-void _app(DomBuilder<Element, Event> b) {
-  b.open(
-    'button',
-    id: 'app-button',
-    events: {
-      'click': (e) {
-        _counter++;
-        e.view.invalidate();
+  void build(DomBuilder<Element, Event> b) {
+    b.open(
+      'button',
+      id: 'app-button',
+      events: {
+        'click': (e) {
+          _counter++;
+          e.view.invalidate();
+        },
       },
-    },
-  );
-  b.text('Click');
-  b.close();
+    );
+    b.text('Click');
+    b.close();
 
-  b.open('div', id: 'app-count');
-  b.text('Counter: $_counter');
-  b.close();
+    b.open('div', id: 'app-count');
+    b.text('Counter: $_counter');
+    b.close();
+  }
 }
